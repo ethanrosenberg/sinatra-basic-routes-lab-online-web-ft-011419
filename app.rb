bd7238052077f12145c7170f8dbede843eaac23d
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-06 05:23:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-
+    "My name is Ethan"
   end
 
   get '/hometown' do
-
+    "My hometown is Westlake Village"
   end
 
   get '/favorite-song' do
-
+  "My favorite song is never had one!"
   end
 end
